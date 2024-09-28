4fbc023a5cfacffdcb11fee67ddae48ecb8f58a0
Kariman <kariman.hesham@advaeg.com>
2024-09-28 23:29:24 +0300

    first coomit

---
+++ HomeFramewok/HomeFramewok/HomeFramewok.h
@@ -0,0 +1,18 @@
+//
+//  HomeFramewok.h
+//  HomeFramewok
+//
+//  Created by MacBook Pro on 28/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HomeFramewok.
+FOUNDATION_EXPORT double HomeFramewokVersionNumber;
+
+//! Project version string for HomeFramewok.
+FOUNDATION_EXPORT const unsigned char HomeFramewokVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HomeFramewok/PublicHeader.h>
+
+
---
+++ HomeFramewok/HomeFramewok/HomeView.swift
@@ -0,0 +1,14 @@
+//
+//  HOMEVIEW.swift
+//  HOMEFRAMEWORK
+//
+//  Created by MacBook Pro on 28/09/2024.
+//
+
+import Foundation
+
+public struct MarcoValidator{
+    public static func sayHello(){
+        print("sayyyyy hello")
+    }
+}
